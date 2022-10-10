3a6762b7bc941a305cf59be6b5de9810fffb8c92
ShaoTX <shao88014@gmail.com>
2022-10-10 21:53:18 +0800

    chore : Modified format to function

--- main.py
+++ main.py
@@ -4,60 +4,39 @@ import numpy as np
 import time
 
 from utils.toArduino import Control_Arduino, Reset_Arduino
+from utils.img2aruco import To_Aruco
 
 # %%
-device_list = ["Fan", "Heat", "Water", "Shock"]
-device_pin = {device_list[0]: 8, device_list[1]: 9, device_list[2]: 10, device_list[3]: 11}
 device_state = [0, 0, 0, 0]
-
-# Create Dictionary of Aruco
-dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
-
-# Initialize the detector parameters using default values
-parameters = cv2.aruco.DetectorParameters_create()
+timer = 0
 
 # %%
 if __name__ == "__main__":
-
-    Reset_Arduino(device_pin)
-
+    Reset_Arduino()
     cap = cv2.VideoCapture(0)
 
     while (True):
         # 擷取影像
         ret, frame = cap.read()
 
-        # 調整圖片大小
-        frame = cv2.resize(frame, None, fx=0.7, fy=0.7,
-                           interpolation=cv2.INTER_CUBIC)
+        frame, markerCorners, markerIds, rejectedCandidates= To_Aruco(frame)
 
-        # 彩色轉灰階
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # Detect the markers in the image
-        markerCorners, markerIds, rejectedCandidates = cv2.aruco.detectMarkers(
-            gray, dictionary, parameters=parameters)
 
         if markerIds != None:
             device_id = int(markerIds[0][0])
-
             print(device_id)
 
-            for i in range(4):
-                if(i == device_id): 
-                    device_state[i] = 1
-                else:
-                    device_state[i] = 0
+            device_state = [0]*4 # [0, 0, 0, 0]
+            device_state[device_id] = 1
+           
         else :
-            print("ya")
-            for j in range(4):
-                device_state[j] = 0
+            print("None")
+            device_state = [0] * 4
 
-        Control_Arduino(device_pin, device_state)
-
-        cv2.aruco.drawDetectedMarkers(frame, markerCorners, markerIds)
+        Control_Arduino(device_state)
 
         # 顯示圖片
+        cv2.aruco.drawDetectedMarkers(frame, markerCorners, markerIds)
         cv2.imshow('live', frame)
 
         # 按下 q 鍵離開迴圈
@@ -68,4 +47,4 @@ if __name__ == "__main__":
     cap.release()
     cv2.destroyAllWindows()
 
-    Reset_Arduino(device_pin)
+    Reset_Arduino()
--- utils/toArduino.py
+++ utils/toArduino.py
@@ -2,19 +2,22 @@ from pyfirmata2 import Arduino
 
 port = 'COM5'
 board = Arduino(port)
+
+device_pin = {"Fan": 8, "Heat": 9, "Water": 10, "Vibe": 11}
+
 print(board)
 
-def Control_Arduino(device_pin: dict, device_state: list):
+def Control_Arduino(device_state: list):
     # 要輸出的裝置
     board.digital[device_pin["Fan"]].write(device_state[0])
     board.digital[device_pin["Heat"]].write(device_state[1])
     board.digital[device_pin["Water"]].write(device_state[2])
-    board.digital[device_pin["Shock"]].write(device_state[3])
+    board.digital[device_pin["Vibe"]].write(device_state[3])
 
 
-def Reset_Arduino(device_pin: dict):
+def Reset_Arduino():
     # 重置所有輸出
     board.digital[device_pin["Fan"]].write(0)
     board.digital[device_pin["Heat"]].write(0)
     board.digital[device_pin["Water"]].write(0)
-    board.digital[device_pin["Shock"]].write(0)
+    board.digital[device_pin["Vibe"]].write(0)
